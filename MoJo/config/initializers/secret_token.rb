79595ee874d933b940a64c99d82970b20f6e8459
Antonio Perez de Tejada <antypdet@gmail.com>
2011-09-05 14:18:00 -0500

    First Commit, Rails Skeleton

---
+++ MoJo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoJo::Application.config.secret_token = '5467f9bba7c5797fd9f04f2aa09d66de6ead6800334bbee64b288a4f6e736d5cd60261b744a72acd631e306a903cd008b822eb86075f2870fb8d5b69888e9982'
