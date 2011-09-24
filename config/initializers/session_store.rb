5cb13a252035cc07da9c0f5687b43fbf91f621c0
Antonio Perez de Tejada <antypdet@gmail.com>
2011-09-24 16:25:06 -0500

    Changed session store to use the database instead of cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Ipro369MoJo::Application.config.session_store :cookie_store, key: '_Ipro369-MoJo_session'
+#Ipro369MoJo::Application.config.session_store :cookie_store, key: '_Ipro369-MoJo_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Ipro369MoJo::Application.config.session_store :active_record_store
+Ipro369MoJo::Application.config.session_store :active_record_store
