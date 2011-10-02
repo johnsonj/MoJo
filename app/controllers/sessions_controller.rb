28e837c94a36e8b8897827e0362f4c4ad97010a7
Antonio Perez de Tejada <antypdet@gmail.com>
2011-10-01 22:44:51 -0500

    thank god, the dirty hack is over

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,7 +21,7 @@ class SessionsController < ApplicationController
       }
       format.json {
         usr = User.find_by_username(params[:username])
-        if usr && usr.authenticate(params[:password]) #&& hasAccess(:app, params[:appKey])
+        if usr && usr.authenticate(params[:password]) && hasAccess(:app, params[:appKey])
           usr.api_key = Digest::SHA1.hexdigest(usr.email + usr.username + Time.now.to_s + Random.rand(1001).to_s)
           usr.save
           render :json => usr.api_key, :status => :ok
