c03dac286163bcf3b6929fe9f60664f0f97b1685
Jeff Johnson <johnsonjeff@gmail.com>
2011-10-05 13:17:09 -0700

    Hushing request tests, moving them to pending

--- spec/requests/item_histories_spec.rb
+++ spec/requests/item_histories_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 describe "ItemHistories" do
   describe "GET /item_histories" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get item_histories_path
-      response.status.should be(200)
+    it "should deny access without authentication" 
     end
   end
 end
