ec63fb8d5c2396c4c2064958c1c40d11e36acb14
Jeff Johnson <johnsonjeff@gmail.com>
2011-10-06 07:36:47 -0700

    Fixing broken test: Route change pingLocation -> recordLocation

--- spec/routing/locations_routing_spec.rb
+++ spec/routing/locations_routing_spec.rb
@@ -3,8 +3,8 @@ require "spec_helper"
 describe LocationsController do
   describe "api routing" do
   
-    it "routes to locationPing" do
-      get("/api/locationPing").should route_to("locations#record_location", :format => "json")
+    it "routes to recordLocation" do
+      get("/api/recordLocation").should route_to("locations#record_location", :format => "json")
     end
   end
 end
