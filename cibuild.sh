7799acec83dd3ca19249601e29fc8f157fe7d5cc
Jeff Johnson <johnsonjeff@gmail.com>
2011-10-05 22:29:24 -0700

    Fix typeo in script

--- cibuild.sh
+++ cibuild.sh
@@ -3,6 +3,6 @@
 nice bundle update
 nice bundle exec rake db:migrate
 nice bundle exec rake spec
-if [ -n "${CC_BUILD_ARTIFACTS+x}"]; then
+if [ -n "${CC_BUILD_ARTIFACTS+x}" ]; then
   cp -r coverage ${CC_BUILD_ARTIFACTS}
 fi
