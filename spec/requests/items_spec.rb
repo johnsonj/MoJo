a8951a3c7b83687a6e5c42028868af21a3adbc04
Jeff Johnson <jjohns14@hawk.iit.edu>
2011-10-18 13:00:51 -0700

    Added tests for /api/pickupItem

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe "API" do
   def valid_api_params
-    app_usr = Factory(:Iphone)
-    usr = Factory(:user)
-    {:appKey => app_usr.api_key, :apiKey => usr.api_key}
+    @app_usr = Factory(:Iphone)
+    @usr = Factory(:user)
+    {:appKey => @app_usr.api_key, :apiKey => @usr.api_key}
   end
   
   describe "getItemsNearBy" do
@@ -43,10 +43,61 @@ describe "API" do
           key["item_description_id"].should == search.first[:item_description_id]
         }
       end
-   
-  
-      
    end
+  end
+  
+  describe "pickupItem" do
+     before (:each) do
+        @coordinates = {:latitude => 1, :longitude => 1}
+        itemdesc = Factory(:item_description)
+        @item1 = Factory(:item_global_1)
+        @item2 = Factory(:item_global_2)
+        @item3 = Factory(:item_owned_1)
+      end
+      
+      describe "with invalid coordinates" do
+        before (:each) do
+          @coordinates = {:latitude => 200, :longitude => 200}
+          get '/api/pickupItem', valid_api_params.merge(@coordinates).merge(:id => @item1.id)
+        end
+
+        it "should return blank for invalid coordinates" do
+          response.body.should == "-1"
+        end
+         it "should not create a new location object" do
+            Location.where(@coordinates).should == []
+         end
+     end
+     describe "with valid coordinates" do
+       describe "with a world owned item" do
+         it "should fail when not near by" do
+           @coordinates = {:latitude => 40, :longitude => 40}
+           get '/api/pickupItem', valid_api_params.merge(@coordinates).merge(:id => @item1.id)
+           response.body.should == "-1"
+         end
+         describe "when near by" do
+           before (:each) do
+             get '/api/pickupItem', valid_api_params.merge(@coordinates).merge(:id => @item1.id)
+           end
+           it "should return sucessful" do
+             response.body.should == "1"
+           end
+           it "should change the item to owned" do
+             search = Item.where(:id => @item1.id)
+             search.should exist
+             search.first[:user_id].should == @usr.id
+           end
+           it "should create item history"
+         end
+       end
+       describe "with a user owned item" do
+         it "should fail" do
+           get '/api/pickupItem', valid_api_params.merge(@coordinates).merge(:id => @item3.id)
+           response.body.should == "-1"
+         end
+       end
+       
+     end
   end
 end
 
