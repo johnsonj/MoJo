5676760ba0b21890e03bb2c35d1da359855abf7a
U-Awesome\Diana <Diana@Awesome.(none)>
2011-10-17 22:43:49 -0500

    another precomppile attempt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ Ipro369MoJo::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+config.assets.precompile += %w( backpack.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
