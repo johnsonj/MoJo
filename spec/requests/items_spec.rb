776914dab781b1a6c3d3f5531baf67da37536240
Jeff Johnson <jjohns14@hawk.iit.edu>
2011-10-20 17:15:21 -0700

    Added initial tests to dropItem

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe "API" do
-  def valid_api_params
+  before(:each) do
     @app_usr = Factory(:Iphone)
     @usr = Factory(:user)
-    {:appKey => @app_usr.api_key, :apiKey => @usr.api_key}
+    @valid_api_params = {:appKey => @app_usr.api_key, :apiKey => @usr.api_key}
   end
   
   describe "getItemsNearBy" do
     describe "with invalid coordinates" do
       before (:each) do
         @coordinates = {:latitude => 200, :longitude => 200}
-        get '/api/getItemsNearBy', valid_api_params.merge(@coordinates)
+        get '/api/getItemsNearBy', @valid_api_params.merge(@coordinates)
       end
       
       it "should return blank for invalid coordinates" do
@@ -29,7 +29,7 @@ describe "API" do
         item2 = Factory(:item_global_2)
         @item_ids = [item1[:id], item2[:id]]
         item3 = Factory(:item_owned_1)
-        get '/api/getItemsNearBy', valid_api_params.merge(@coordinates)
+        get '/api/getItemsNearBy', @valid_api_params.merge(@coordinates)
         @result = JSON.parse(response.body)
       end
       it "should return a list of world owned items" do
@@ -58,7 +58,7 @@ describe "API" do
       describe "with invalid coordinates" do
         before (:each) do
           @coordinates = {:latitude => 200, :longitude => 200}
-          get '/api/pickupItem', valid_api_params.merge(@coordinates).merge(:id => @item1.id)
+          get '/api/pickupItem', @valid_api_params.merge(@coordinates).merge(:id => @item1.id)
         end
 
         it "should return blank for invalid coordinates" do
@@ -72,12 +72,12 @@ describe "API" do
        describe "with a world owned item" do
          it "should fail when not near by" do
            @coordinates = {:latitude => 40, :longitude => 40}
-           get '/api/pickupItem', valid_api_params.merge(@coordinates).merge(:id => @item1.id)
+           get '/api/pickupItem', @valid_api_params.merge(@coordinates).merge(:id => @item1.id)
            response.body.should == "-1"
          end
          describe "when near by" do
            before (:each) do
-             get '/api/pickupItem', valid_api_params.merge(@coordinates).merge(:id => @item1.id)
+             get '/api/pickupItem', @valid_api_params.merge(@coordinates).merge(:id => @item1.id)
            end
            it "should return sucessful" do
              response.body.should == "1"
@@ -92,12 +92,51 @@ describe "API" do
        end
        describe "with a user owned item" do
          it "should fail" do
-           get '/api/pickupItem', valid_api_params.merge(@coordinates).merge(:id => @item3.id)
+           get '/api/pickupItem', @valid_api_params.merge(@coordinates).merge(:id => @item3.id)
            response.body.should == "-1"
          end
        end
        
      end
+  end
+  
+  describe "dropItem" do
+     before (:each) do
+        @item_global_1 = Factory(:item_global_1)
+        @item_global_2 = Factory(:item_global_2)
+        @item_owned_1 = Factory(:item_owned_1, :user_id => @usr.id) 
+     end
+    describe "with valid coordinates" do
+      def coordinates
+        {:latitude => 1, :longitude => 1}
+      end
+      describe "with a valid, user owned item" do
+      def params
+        @valid_api_params.merge(coordinates).merge(:id => @item_owned_1.id, :signature => "Hey there")
+      end
+        before (:each) do
+          get '/api/dropItem', params        
+        end
+        it "should return sucessful" do
+          response.body.should == "1"
+        end
+        it "should assign the item to the world" do
+          Item.where(:id => @item_owned_1.id).first.user_id.should == 0
+        end
+        it "should create item history" do
+          history = ItemHistory.where(:item_id => @item_owned_1.id).first
+          history.should_not == nil
+          history.signature.should == params[:signature]
+          history.latitude.should == params[:latitude]
+          history.longitude.should == params[:longitude]
+        end
+        it "should create item history with a valid distance traveled"
+      end
+      describe "with a world owned item" do
+        it "should not return sucessful"
+        it "should not create item history"
+      end
+    end
   end
 end
 
