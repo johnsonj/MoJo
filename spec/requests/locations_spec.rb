1d804a6576b20dc2e4b0c54a1c1ba89750c9ac99
U-Awesome\Diana <Diana@Awesome.(none)>
2011-10-24 12:29:07 -0500

    API/getLocationForRange tests added

--- spec/requests/locations_spec.rb
+++ spec/requests/locations_spec.rb
@@ -56,4 +56,46 @@ describe "API" do
     end
 
 
+   describe "getLocationsForRangeInArea" do
+       describe "with valid parameters" do
+          before(:each) do
+            @start_end = {:start => "2010-01-01T11:00:00Z", :end => "2010-01-01T13:00:00Z", :startingLongitude => "5", startingLatitude => "4", endingLongitude => "6", endingLatitude => "5"}
+            @valid_params = @start_end.merge(@valid_api_key)
+          end
+          describe "locations within coordinates and time range" do
+             describe "having count=true" do
+                it "should return correct count" do
+                   get 'api/getLocationsForRangeInArea', @valid_params.merge(:count => "true")
+                   response.body.should be == "1"
+                end
+             end
+             before(:each) do
+                @location_ids_in_list = [@location2[:id]]
+                @location_ids_not_in_list = [@location1[:id], @location3[:id], @location4[:id], @location5[:id], @location6[:id]]
+                get 'api/getLocationsForRangeInArea', @valid_params.merge(:count => "false")
+                @result = JSON.parse(response.body)
+             end
+             describe "having count = false" do
+                it "should include correct locations" do
+                   @result.each{|key| key["id"].should be_in @location_ids_in_list }
+                end
+                it "should not include incorrect locations" do
+                   @result.each{|key| key["id"].should_not be_in @location_ids_not_in_list }
+                end
+             end
+          end
+    
+       end
+       describe "with invalid parameters" do
+          describe "invalid range" do
+    
+             it "should return empty list" do
+            @start_end = {:start => "2010-01-01T11:00:00Z", :end => "2010-01-01T13:00:00Z", :startingLongitude => "8", startingLatitude => "40", endingLongitude => "6", endingLatitude => "5"}
+                @valid_params = @start_end.merge(@valid_api_key)
+                get 'api/getLocationsForRangeInArea', @valid_params.merge(:count => "false")
+                JSON.parse(response.body).should be == []
+             end
+          end
+       end
+   end
 end
