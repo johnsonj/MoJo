2f7580e71682c72fb6126b925d14946d91b838a0
Anirudh <anirudh.ur8@gmail.com>
2011-10-24 17:14:50 -0500

    write interaction test

--- spec/requests/interactions_spec.rb
+++ spec/requests/interactions_spec.rb
@@ -2,22 +2,46 @@ require 'spec_helper'
 
 describe "API" do
   before(:each) do
-      @app_usr = Factory(:Iphone)
-      @usr = Factory(:user)
-      @valid_api_params = {:appKey => @app_usr.api_key, :apiKey => @usr.api_key}
+      @app_usr = Factory(:user_app_interactions)
+      @user2 = Factory(:Iphone)
+      @valid_api_params = {:appKey => "c7496b766f2ec767b3644b3b9cc51147a97b83ce"}
+      @interaction = Factory(:valid_interaction)
   end
 
   describe "writeInteraction" do
-
-    describe "asd" do
-      it "should fail" do
-	   
-      end 
+    before(:each) do
+     @valid_interaction =  {:Loclatitude => "2", :loclongitude =>"2", :PersonA=>"1" , :PersonB=>"2", :Timestamp=>"2010-01-01T10:00:00Z", :deltime=>"10"}
+     @valid_request =   @valid_api_params.merge(:Loclatitude => "2", :loclongitude =>"2", :PersonA=>"1" , :PersonB=>"2", :Timestamp=>"2010-01-01T10:00:00Z", :deltime=>"10")
     end
+
+    describe "valid parameters" do
+        it "should pass" do
+          get '/api/writeInteraction', @valid_request
+          Interaction.where(@valid_interaction).should exist
+        end
+        end
+
+      describe "invalid latitude" do
+        it "should fail" do
+          get '/api/writeInteraction', @valid_request.merge(:Loclatitude=>"180")
+          Interaction.where(@valid_interaction.merge(:Loclatitude=>"180")).should_not exist
+        end
+        end
+
+        describe "invalid longitude" do
+        it "should fail" do
+          get '/api/writeInteraction', @valid_request.merge(:loclongitude=>"360")
+          Interaction.where(@valid_interaction.merge(:loclongitude=>"360")).should_not exist
+        end
+        end
+
+        describe "invalid Timestamp" do
+        it "should fail" do
+          get '/api/writeInteraction', @valid_request.merge(:Timestamp=>"1200")
+          Interaction.where(@valid_interaction.merge(:Timestamp=>"1200")).should_not exist
+        end
+        end
   end
-
-
-
-end
+  end
 
 
--- spec/requests/locations_spec.rb
+++ spec/requests/locations_spec.rb
@@ -2,8 +2,26 @@ require 'spec_helper'
 
 describe "API" do
   before(:each) do
-	
+	  @loc1 = Factory(:location_set_1)
+    @loc2 = Factory(:location_set_2)
+    @loc3 = Factory(:location_set_3)
+    @loc4 = Factory(:location_set_4)
+    @loc5 = Factory(:location_set_5)
+    @loc6 = Factory(:location_set_6)
+
   end
 
+  describe "getLocationsInRange" do
+    before(:each) do
+    @valid_request =  (:Loclatitude => "2", :loclongitude =>"2", :PersonA=>"1" , :PersonB=>"2", :Timestamp=>"2010-01-01T10:00:00Z", :deltime=>"10")
+    end
+
+    describe "valid parameters" do
+        it "should pass" do
+          get '/api/writeInteraction', @valid_request
+          Interaction.where(@valid_interaction).should exist
+        end
+        end
+    end
 
 end
