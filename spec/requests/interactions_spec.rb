9d4384644bdb40d467f140c941f50b6869bfaab4
U-Awesome\Diana <Diana@Awesome.(none)>
2011-10-23 20:43:40 -0500

    syntax errors

--- spec/requests/interactions_spec.rb
+++ spec/requests/interactions_spec.rb
@@ -7,4 +7,17 @@ describe "API" do
       @valid_api_params = {:appKey => @app_usr.api_key, :apiKey => @usr.api_key}
   end
 
+  describe "writeInteraction" do
+
+    describe "asd" do
+      it "should fail" do
+	   
+      end 
+    end
+  end
+
+
+
+end
+
 
--- spec/requests/locations_spec.rb
+++ spec/requests/locations_spec.rb
@@ -2,9 +2,8 @@ require 'spec_helper'
 
 describe "API" do
   before(:each) do
-      @app_usr = Factory(:Iphone)
-      @usr = Factory(:user)
-      @valid_api_params = {:appKey => @app_usr.api_key, :apiKey => @usr.api_key}
+	
   end
 
 
+end
