4ef3b357347c3720fd7dce03977d452d283e813c
Anirudh <anirudh.ur8@gmail.com>
2011-10-24 23:58:04 -0500

    logout test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -105,44 +105,33 @@ describe "API" do
   end
 
   describe "logout" do
-    describe "when logging out of the api from the mobile app" do
-        def app_params
+	def app_params
           {:appKey => @app_usr.api_key}
         end
-        describe "with valid credentials" do
-          def params
+	def params
             app_params.merge(:username => @usr.username, :password => @usr.password)
           end
-          before (:each) do
+    before (:each) do
             get '/api/login', params
+            @key2 = response.body
           end
-          it "should return a valid api key" do
-            result = response.body
-            result.should == User.where(:id => @usr.id).first.api_key
+    describe "when logging out of the api from the mobile app" do
+        describe "with valid api key" do
+          it "should pass" do
+            get '/api/logout',  {:apiKey => @key2}
+            response.status.should == 200
+            User.where(:api_key => @key2).should_not exist
           end
-      end
-      define "with invalid credentials" do
-        it "should not succeed"
-      end
-    end
-    describe "when logging in with an invalid app key" do
-      def app_params
-        {:appKey => "invalid!" }
-      end
-      describe "with valid credentials" do
-        it "should not succeed" do
-          get '/api/login', app_params.merge(:username => @usr.username, :password => @usr.password)
-          response.status.should_not be :ok
-          end
-      end
-      describe "invalid credentials" do
-        it "should not succeed" do
-          get '/api/login', app_params.merge(:username => "blah", :password => "no")
-          response.status.should_not be :ok
         end
+
+      describe "with invalid api key" do
+          it "should fail" do
+            get '/api/logout', {:apiKey => "random"}
+            response.status.should == 404
+            User.where(:api_key => @key2).should exist
+          end
       end
-    end
+        end
   end
-
-
-end
+  end
+ 
