1355a4ec668469c87188d215f34daabef01eaae6
Udit Ramawat <udit.ramawat@gmail.com>
2011-10-27 14:13:46 -0500

    committing to pull latest changes
    These are a bunch of unsaved edits

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Ipro369MoJo::Application.routes.draw do
   match '/login' => 'sessions#new', :as => :login_path
   match '/logout' => 'sessions#destroy', :as => :logout_path
 
-  match '/backpack' => 'items#backpack'
+    match '/backpack' => 'items#backpack', :as => :backpack_path
   match '/itemDetails/:id' => 'item_histories#itemDetails'
 
   ### API Methods ###
