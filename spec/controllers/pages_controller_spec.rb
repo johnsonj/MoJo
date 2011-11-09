e80900da8863e5dce0174a491839f7be9b3a61e9
Antonio Perez de Tejada <antypdet@gmail.com>
2011-11-08 19:41:30 -0600

    Leaderboards need you to be logged in

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
 
+  before(:each) do
+    log_in_test Factory(:user)
+  end
+
   describe "GET 'leaderboards'" do
     it "should be successful" do
       get 'leaderboards'
